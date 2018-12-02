be3dc1138abf7769a596e0ac5072a8475b6428b4
ChorriCode <compartelo007@yahoo.es>
2018-12-02 19:09:23 +0000

    nuevas implentaciones en ServletLibro

--- src/main/java/controller/servlet/ServletLibro.java
+++ src/main/java/controller/servlet/ServletLibro.java
@@ -1,6 +1,8 @@
 package controller.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Libro;
+import model.modelDAO.LibroDAO;
+
 /**
  * Servlet implementation class ServletController
  */
-@WebServlet(description = "Controlador Gestion Libros", urlPatterns = { "/ServletControllerLibro" })
+@WebServlet(description = "Controlador Gestion Libros", urlPatterns = { "/ServletLibro" })
 public class ServletLibro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	HttpServletRequest request;
+	HttpServletResponse response;
+	LibroDAO myBookDAO = new LibroDAO();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -34,11 +42,28 @@ public class ServletLibro extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String libroId = request.getParameter("libroId");
-		System.out.println(libroId);
-		response.getWriter().append("Served at: ").append(request.getContextPath() + " - " + libroId);
+		this.request = request;
+		this.response = response;
+		String option = request.getParameter("option");
+		String path = "";
+
+		
+		switch (option) {
+			case "findId":
+				int bookId = Integer.parseInt(request.getParameter("libroId"));
+				Libro oneBook = findBookById(bookId);
+				request.setAttribute("book", oneBook);
+				path = "jsp/bookDetails.jsp";
+				break;
+	
+			default:
+				break;
+		}
+		RequestDispatcher rs = request.getRequestDispatcher(path);
+		rs.forward(request, response);
 	}
+
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -48,4 +73,13 @@ public class ServletLibro extends HttpServlet {
 		doGet(request, response);
 	}
 
+	// este método del controlador lláma al método DAO encargado de buscar la información de un Libro en concreto
+	//en la BBDD cuando lo encuentra lo devuelve
+	private Libro findBookById(int bookId) {
+		return myBookDAO.getLibroPorId(bookId);
+		
+	}
+	
+
+	
 }
---
+++ src/main/webapp/jsp/bookDetails.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Book details</title>
+</head>
+<body>
+	<h1>Book details</h1>
+	<div>
+		<p>id: ${book.id}</p>
+		<p>Título: ${book.titulo}</p>
+		<p>Autor: ${book.autor}</p>
+		<p>Precio: ${book.precio}</p>
+	</div>
+</body>
+</html>
